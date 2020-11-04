859fbbc247ed094eda21d9ef9c829e9c857f91c1
tangkeqin857 <1246743136@qq.com>
2020-11-05 05:05:17 +0800

    search completed

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Search from 'antd/lib/input/Search';
 import './style.css'
-const onSearch = value => console.log(value);
+import store from '../../store';
+import { notification } from 'antd';
+
+const openNotification = (value) => {
+  notification.open({
+    message: '搜索结果',
+    description: 
+    
+    <Fragment>
+      {
+        value.map((item, index)=>(<div key={index}>{item}</div>))
+      }
+    </Fragment>,
+    onClick: () => {
+      console.log('Notification Clicked!');
+    },
+  });
+};
+
+const onSearch = value => {
+  let state = store.getState();
+  let str = [];
+  state.list.filter((item)=> {
+    return item.content.indexOf(value) !== -1
+  }).forEach((item)=> {
+    str.push(`${item.content} ${item.time} ${item.status ? '已完成' : '未完成'}`);
+  });
+  openNotification(str);
+};
 
 const SearchInput = (props) => {
   return (
